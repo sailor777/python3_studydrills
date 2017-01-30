dce564764c0839b1a1131bbe8a5ec02e35ef36b8
Pavlo Komendant <pavlo.ok@gmail.com>
2017-01-30 16:53:36 +0200

    ex05 commit studydrills

--- ex05-studydrills.py
+++ ex05-studydrills.py
@@ -3,16 +3,16 @@
 # ex5: More Variables and Printing
 
 name = 'Zed A. Shaw'
-age = 35      # not a lie
-height = 74   # inches
-weight = 180  # lbs
-eyes = "Blue"
+age = 36      # not a lie
+height = 172  # cm
+weight = 60  # kg
+eyes = "Green"
 teeth = 'White'
-hair = 'Brown'
+hair = 'Red'
 
 print("Let's talk about %s" % name)
-print("He's %d inches tall." % height)
-print("He\'s %d pounds heavy." % weight)
+print("He's %d centimeters tall." % height)
+print("He\'s %d kilogram heavy." % weight)
 print("He's got %s eyes and %s hair." % (eyes, hair))
 print("Actually that's not too heavy")
 print("His teeth are usually %s depending on the coffee." % teeth)
@@ -21,16 +21,16 @@ print("His teeth are usually %s depending on the coffee." % teeth)
 print('If I add %d, %d and %d I get %d.' % (age, height, weight, age + height + weight))
 
 # try more format characters
-my_greeting = "Hello,\t"
-my_first_name = 'Joseph'
-my_last_name = 'Pan'
-my_age = 24
+greeting = "Hello,\t"
+first_name = 'Danko'
+last_name = 'Yanko'
+age = 22
 # Print 'Hello,\t'my name is Joseph Pan, and I'm 24 years old.
-print("%r my name is %s %s, and I'm %d years old." % (my_greeting, my_first_name, my_last_name, my_age))
+print("%r my name is %s %s, and I'm %d years old." % (greeting, first_name, last_name, age))
 
 # Try to write some variables that convert the inches and pounds to centimeters and kilos.
 inches_per_centimeters = 2.54
 pounds_per_kilo = 0.45359237
 
-print("He's %f centimeters tall." % (height * inches_per_centimeters))
-print("He's %f kilos heavy." % (weight * pounds_per_kilo))
+print("He's %f inches tall." % (height / inches_per_centimeters))
+print("He's %f pounds heavy." % (weight / pounds_per_kilo))
